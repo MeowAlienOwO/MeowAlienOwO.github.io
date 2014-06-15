bd77cee105817d2569982a4a68c62529235c1a32
Zhang Huayan <meowalienowo@gmail.com>
2014-06-15 23:03:12 +0800

    set menu

---
+++ myFunctions.js
@@ -0,0 +1,58 @@
+function writeHeader(){
+    document.write('<div id="banner">');
+        document.write('<img src="http://meowalienowo.github.io/img/9516952.jpg" />');
+            document.write('</div>');
+            document.write('<!-- end of header --> ');
+            document.write('<!-- menu -->');
+            document.write('<div class="menu">');
+                document.write('<ul>');
+                    document.write('<li>');
+                        document.write('<a href="http://meowalienowo.github.io/notes.html" id="enter" title="notes">Notes</a>');
+                        document.write('<ul>');
+                            document.write('<li><a href="">Online Notes</a></li>');
+                            document.write('<li><a href="">Reading</a></li>');
+                            document.write('<li><a href="">Other Things</a></li>');
+                        document.write('</ul>');
+                    document.write('</li>');
+                    document.write('<li>');
+                        document.write('<a href="http://meowalienowo.github.io/helloworld.html" id="enter" title="helloworld">HelloWorld</a>');
+                        document.write('<ul>');
+                            document.write('<li><a href="">C/C++</a></li>');
+                            document.write('<li><a href="">Java</a></li>');
+                            document.write('<li><a href="">Algorithm</a></li>');
+                            document.write('<li><a href="">Linux</a></li>');
+                        document.write('</ul>');
+                    document.write('</li>');
+                    document.write('<li>');
+                        document.write('<a href="http://meowalienowo.github.io/projects.html" id="enter" title="projects">Project</a>');
+                        document.write('<ul>');
+                            document.write('<li><a href="">掀桌Game</a></li>');
+                        document.write('</ul>');
+                    document.write('</li>');
+                    document.write('<li><a href="http://meowalienowo.github.io/life.html" id="enter" title="life">Life</a></li>');
+                    document.write('<li><a href="http://meowalienowo.github.io/martialart.html" id="enter" title="martial art">Martial Art</a></li>');
+                    document.write('<li>');
+                        document.write('<a href="http://meowalienowo.github.io/language.html" id="enter" title="language">Language</a>');
+                        document.write('<ul>');
+                            document.write('<li><a href="">English</a></li>');
+                            document.write('<li><a href="">日本語</a></li>');
+                        document.write('</ul>');
+                    document.write('</li>');
+                    document.write('<li>');
+                        document.write('<a href="http://meowalienowo.github.io/aboutme.html" id="enter" title="about me">About me</a>');
+                    document.write('</li>');
+                    document.write('<li><a href="http://meowalienowo.github.io/index.html" id="enter" title="back">Back</a></li>');
+                document.write('</ul>');
+                document.write('<br />');
+            document.write('</div>');
+
+}
+
+function writeFooter(){
+
+        document.write('<p><a href="#">Top</a></p>');
+        document.write('<p><a href="mailto:meowalien@gmail.com">Email: meowalienowo@gmail.com</a></p>');
+        document.write('<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">知识共享署名-相同方式共享 4.0 国际许可协议</a>进行许可。');
+
+
+}
