63f6b4e1ed4df2c21aa25f9fd8454c453899ebe1
Meow <meowalienowo@gmail.com>
2015-02-27 13:01:45 +0800

    update source

---
+++ .hyde.el
@@ -0,0 +1,32 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;; -*- Mode: Emacs-Lisp -*- ;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; .hyde.el --- 
+;; Filename: .hyde.el
+;; Description: 
+;; Author: Zhang Huayan
+;; ID number: 6511043
+;; E-mail: zy11043@nottingham.edu.cn / MeowAlienOwO@gmail.com
+;; Version: 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Commentary: 
+;; 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Change Log:
+;; Status: 
+;; Table of Contents: 
+;; 
+;;     Update #: 5
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 
+;;; Code:
+
+(setq hyde/git/remote "origin" ; The name of the remote to which we should push
+      hyde/git/remote-branch "master"   ; The name of the branch on which your blog resides
+      hyde/deploy-command  "make deploy" ; Command to deploy
+      )
+
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; .hyde.el ends here
---
+++ Makefile
@@ -0,0 +1,18 @@
+
+deploy:
+	git checkout source
+	jekyll build
+	git add -A
+	git commit -m "update source"
+	cp -r _site/ /tmp/
+	git checkout master 
+	rm -r ./*
+	cp -r /tmp/_site/* ./
+	git add -A
+	git commit -m "update page infor"
+	echo "deploy finished"
+	git push origin master 
+	git checkout source
+	git push origin source
+	echo "push finished"    
+
