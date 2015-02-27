a8e4821d74bd9a38b7e3bf47a454dcb9c5c7608e
Meow <meowalienowo@gmail.com>
2015-02-27 17:30:14 +0800

    update page infor

---
+++ css/myFunctions.js
@@ -0,0 +1,60 @@
+function writeHeader(){
+    document.write('<div id="banner">');
+        document.write('<img src="http://meowalienowo.github.io/img/9516952.jpg" />');
+            document.write('</div>');
+            document.write('<!-- end of header --> ');
+            document.write('<!-- menu -->');
+            document.write('<div class="menu">');
+                document.write('<ul>');
+                    document.write('<li>');
+                        document.write('<a href="http://meowalienowo.github.io/entry/notes.html" id="enter" title="notes">Notes</a>');
+                        document.write('<ul>');
+                            document.write('<li><a href="http://meowalienowo.github.io/article/notes/OnlineNotes.html">Online Notes</a></li>');
+                            document.write('<li><a href="http://meowalienowo.github.io/article/notes/OtherThings.html">Other Things</a></li>');
+                            document.write('<li><a href="http://meowalienowo.github.io/article/notes/Reading.html">Reading</a></li>');
+                        document.write('</ul>');
+                    document.write('</li>');
+                    document.write('<li>');
+                        document.write('<a href="http://meowalienowo.github.io/entry/helloworld.html" id="enter" title="helloworld">HelloWorld</a>');
+                        document.write('<ul>');
+                            document.write('<li><a href="http://meowalienowo.github.io/article/helloworld/C_Cpp.html">C/C++</a></li>');
+                            document.write('<li><a href="http://meowalienowo.github.io/article/helloworld/Java.html">Java</a></li>');
+                            document.write('<li><a href="http://meowalienowo.github.io/article/helloworld/Algorithm.html">Algorithm</a></li>');
+                            document.write('<li><a href="http://meowalienowo.github.io/article/helloworld/Linux.html">Linux</a></li>');
+                        document.write('</ul>');
+                    document.write('</li>');
+                    document.write('<li>');
+                        document.write('<a href="http://meowalienowo.github.io/entry/projects.html" id="enter" title="projects">Project</a>');
+                        document.write('<ul>');
+                            document.write('<li><a href="http://lshy1993.github.io/">掀桌Game</a></li>');
+                        document.write('</ul>');
+                    document.write('</li>');
+                    document.write('<li><a href="http://meowalienowo.github.io/entry/life.html" id="enter" title="life">Life</a></li>');
+                    document.write('<li><a href="http://meowalienowo.github.io/entry/martialart.html" id="enter" title="martial art">Martial Art</a></li>');
+                    document.write('<li>');
+                        document.write('<a href="http://meowalienowo.github.io/entry/language.html" id="enter" title="language">Language</a>');
+                        document.write('<ul>');
+                            document.write('<li><a href="http://meowalienowo.github.io/article/language/English">English</a></li>');
+                            document.write('<li><a href="http://meowalienowo.github.io/article/language/Japanese">日本語</a></li>');
+                        document.write('</ul>');
+                    document.write('</li>');
+                    document.write('<li>');
+                        document.write('<a href="http://meowalienowo.github.io/entry/aboutme.html" id="enter" title="about me">About me</a>');
+                    document.write('</li>');
+                    document.write('<li><a href="http://meowalienowo.github.io/entry/index.html" id="enter" title="back">Back</a></li>');
+                document.write('</ul>');
+                document.write('<br />');
+            document.write('</div>');
+
+}
+
+function writeFooter(){
+
+        document.write('<p><a href="#">Top</a></p>');
+        document.write('<p><a href="mailto:meowalien@gmail.com">Email: meowalienowo@gmail.com</a></p>');
+        document.write('<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/"><img alt="知识共享许可协议" style="border-width:0" src="http://i.creativecommons.org/l/by-sa/4.0/88x31.png" /></a><br />本作品采用<a rel="license" href="http://creativecommons.org/licenses/by-sa/4.0/">知识共享署名-相同方式共享 4.0 国际许可协议</a>进行许可。');
+
+
+}
+
+
